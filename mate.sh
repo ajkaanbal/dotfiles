0679f13d4c8b56951bb80f1d8acf0bfe91a93d6a
Ricardo M. Vilchis <ajkaanbal@gmail.com>
2014-03-17 09:55:57 -0600

    fix mate keybinding

--- mate.sh
+++ mate.sh
@@ -13,7 +13,7 @@ gsettings set org.mate.Marco.global-keybindings switch-to-workspace-up '<Control
 gsettings set org.mate.Marco.global-keybindings switch-to-workspace-down '<Control><Alt>j'
 
 #Move window to workspace
-gsettings set org.mate.Marco.window-keybindings move-to-workspace-down '<Control><Shift><Alt>k'
-gsettings set org.mate.Marco.window-keybindings move-to-workspace-up '<Control><Shift><Alt>j'
+gsettings set org.mate.Marco.window-keybindings move-to-workspace-down '<Control><Shift><Alt>j'
+gsettings set org.mate.Marco.window-keybindings move-to-workspace-up '<Control><Shift><Alt>k'
 gsettings set org.mate.Marco.window-keybindings move-to-workspace-left '<Control><Shift><Alt>h'
 gsettings set org.mate.Marco.window-keybindings move-to-workspace-right '<Control><Shift><Alt>l'
